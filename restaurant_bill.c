c133c81f01c7ce4b38cc4156ffc76bf39ab6f618
AN LEEK <lkbaoan@gmail.com>
2022-10-22 13:51:07 -0700

    Update menu

--- restaurant_bill.c
+++ restaurant_bill.c
@@ -14,29 +14,34 @@ int main(int argc, char *argv[]) {
         tip_rate = atof(argv[2]);
     }
 
+    typedef struct {
+    char *name;
+    double price;
+    } Menu;
+
+    const Menu MenuTable[] = {
+        {"Salad", 9.95},
+        {"Soup", 4.55},
+        {"Sandwich", 13.25},
+        {"Pizza", 22.35}
+    };
     // random select 4 meal to choose
     time_t t;
     srand((unsigned)time(&t));
     int choose_menu = rand() % 4;
-    const char menu[4][10] = {
-        "Salad",
-        "Soup",
-        "Sandwich",
-        "Pizza"};
-    double price[4] = {9.95, 4.55, 13.25, 22.35};
     // display meal cost
-    printf("Price of %s: $%.2f\n", menu[choose_menu], price[choose_menu]);
+    printf("Price of %s: $%.2f\n", MenuTable[choose_menu].name, MenuTable[choose_menu].price);
     // display tax amount
     // if no argument default will be 9.75%
     // tax = cost*(tax_rate/100)
-    double tax = price[choose_menu] * (tax_rate / 100);
+    double tax = MenuTable[choose_menu].price * (tax_rate / 100);
     printf("Tax amount: $%.2f\n", tax);
     // display tip amount
     // if no argument default will be 0%
     // tip = cost*(tip_rate/100)
-    double tip = price[choose_menu] * (tip_rate / 100);
+    double tip = MenuTable[choose_menu].price * (tip_rate / 100);
     printf("Tip amount: $%.2f\n", tip);
     // display total cost
-    printf("Total price: $%.2f\n", (price[choose_menu] + tax + tip));
+    printf("Total price: $%.2f\n", (MenuTable[choose_menu].price + tax + tip));
     return EXIT_SUCCESS;
 }
