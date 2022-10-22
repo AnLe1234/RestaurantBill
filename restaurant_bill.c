1edff6a71e4341671669c384da469d9aeeba8cee
AN LEEK <lkbaoan@gmail.com>
2022-10-21 17:33:28 -0700

    Create outline for restaurant bill

---
+++ restaurant_bill.c
@@ -0,0 +1,11 @@
+// program that compute a bill with tax and tip as command line argument
+// main accep 2 argument for tax and tip, more will return fasle if none will use default value
+// random select 4 meal to choose
+// display meal cost
+// display tax amount
+    // if no argument default will be 9.75%
+    // tax = cost*(tax/100)
+// display tip amount
+    // if no argument default will be 0%
+    // tip = cost*(tip/100)
+// display total cost
